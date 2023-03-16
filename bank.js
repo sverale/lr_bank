876ae84e585b5c7496fb4e43e26d3e9f32658ab4
sverale <112919812+sverale@users.noreply.github.com>
2023-03-16 12:33:31 +0700

    Add files via upload

---
+++ bank.js
@@ -0,0 +1,39 @@
+const cal = document.querySelector('.cal');
+const result = document.querySelector('#result');
+
+cal.addEventListener('click', function(event) {
+  if(!event.target.classList.contains('cal_btn')) return;
+
+  const value = event.target.innerText;
+
+ 
+
+////////////////////////////////////////////////
+// class Math {
+//   // методы класса
+//   constructor() { ... }
+
+//   factorial() { ... }
+//   degree() { ... }
+//   sqrt () { ... }
+//   ...
+// }
+///////////////////////////////////////////
+// class User {
+
+//   constructor(name) {
+//     this.name = name;
+//   }
+
+//   sayHi() {
+//     alert(this.name);
+//   }
+
+// }
+
+// // Использование:
+// let user = new User("Иван");
+// user.sayHi();
+////////////////////////////////////////////////
+
+});
